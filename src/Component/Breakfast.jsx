bb3194cda71d3a5bf93acd5b6e286780336464b7
kaarenzu <karenzcortes0211@gmail.com>
2020-04-15 20:30:45 -0400

    Ordené el código

--- src/Component/Breakfast.jsx
+++ src/Component/Breakfast.jsx
@@ -1,56 +1,47 @@
 import React from 'react';
-import {db} from './ConfigFirebase.jsx';
+import { db } from './ConfigFirebase.jsx';
 import '../Css/breakfast.css'
 
 class Breakfast extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       breakfast: [],
-     
     }
   }
-    componentDidMount(){
+  componentDidMount() {
     db.collection('desayuno').get().then((element) => {
       const newBreakfast = element.docs.map(doc => doc.data());
-      this.setState ({
-        breakfast : newBreakfast
+      this.setState({
+        breakfast: newBreakfast
       })
     })
   }
-  render(){
+  render() {
     return (
       <div>
-         <table class="table table-dark">
+        <table class="table table-dark">
           <thead>
             <tr>
               <th scope="col">Nombre</th>
               <th scope="col">Precio</th>
-              <th scope="col">Agregar</th>     
+              <th scope="col">Agregar</th>
             </tr>
           </thead>
-          {this.state.breakfast.map((element, key) =>{
-          return (
-           
-               
-                <tbody>
-                  <tr>
-                     <th scope="row">{element.name}</th>
-                      <td>${element.price}</td>
-                      <td><button className="Agregar">Agregar</button></td>
-                   </tr>
-                </tbody>
-               
-
-          
-          ) 
-        })          
-      }
- 
+          {this.state.breakfast.map((element, key) => {
+            return (
+              <tbody>
+                <tr>
+                  <th scope="row">{element.name}</th>
+                  <td>${element.price}</td>
+                  <td><button className="Agregar">Agregar</button></td>
+                </tr>
+              </tbody>
+            )
+          })
+          }
         </table>
-
-     
-    </div>
+      </div>
     )
   }
 }
--- src/Component/Waiter.jsx
+++ src/Component/Waiter.jsx
@@ -1,68 +1,64 @@
-import React,{Fragment}from 'react';
+import React, { Fragment } from 'react';
 import '../Css/waiter.css';
 import Breakfast from './Breakfast';
 import Lunch from './Lunch.jsx';
 import OrderBox from './OrderBox.jsx';
 
-class Waiter extends React.Component{
-  constructor(){
+class Waiter extends React.Component {
+  constructor() {
     super();
     this.state = {
-      
       desayuno: true,
       lunch: true
-
     }
     this.onclickBreakfast = this.onclickBreakfast.bind(this);
     this.onclickLunch = this.onclickLunch.bind(this);
   }
-  onclickBreakfast(){
+  // Función que muestra/oculta el menú desayuno y oculta almuerzo
+  onclickBreakfast() {
     if (!this.state.desayuno) {
       this.setState({ desayuno: true })
     }
     if (this.state.desayuno) {
       this.setState({ desayuno: false })
     }
+    // Condición que oculta almuerzo
     if (!this.state.lunch) {
       this.setState({ lunch: true })
     }
     console.log(this.state.desayuno, 'mi estado')
   }
-  onclickLunch(){
+  // Función que muestr/oculta el menú almuerzo y oculta desayuno
+  onclickLunch() {
     if (!this.state.lunch) {
       this.setState({ lunch: true })
     }
     if (this.state.lunch) {
       this.setState({ lunch: false })
     }
+    // Condición que oculta desayuno
     if (!this.state.desayuno) {
       this.setState({ desayuno: true })
     }
     console.log(this.state.lunch, 'mi estado')
   }
-  render(){
-   
-      return(<Fragment>
-        
-      <div className="divTotal">
-        <div className="waiter">
-        <button className="btn btn-success btn-lg" id="btn" onClick={this.onclickBreakfast}>Desayuno</button>
-         <button className="btn btn-danger btn-lg" id="btn"onClick={this.onclickLunch}>Almuerzo</button>
-       
+  render() {
+    return (
+      <Fragment>
+        <div className="divTotal">
+          <div className="waiter">
+            <button className="btn btn-success btn-lg" id="btn"
+              onClick={this.onclickBreakfast}>Desayuno</button>
+            <button className="btn btn-danger btn-lg" id="btn"
+              onClick={this.onclickLunch}>Almuerzo</button>
+          </div>
+          <div className="containerTabla">
+            {!this.state.desayuno ? <Breakfast /> : null}
+            {!this.state.lunch ? <Lunch /> : null}
+            <OrderBox />
+          </div>
         </div>
-        <div className="containerTabla">
-        {!this.state.desayuno?<Breakfast/>: null}
-        {!this.state.lunch?<Lunch/>: null}
-        <OrderBox/>
-        </div>
-        
-    </div>
-    </Fragment>)
-
-    
-   
-      
-   
+      </Fragment>)
   }
 }
 export default Waiter;
