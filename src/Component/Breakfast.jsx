351e3d3d83d35f8c78278bf24440edcd08d32590
kaarenzu <karenzcortes0211@gmail.com>
2020-04-21 11:34:10 -0400

    Agregué css de mesero y ordené el codigo

--- src/Component/Breakfast.jsx
+++ src/Component/Breakfast.jsx
@@ -7,20 +7,58 @@ class Breakfast extends React.Component {
     super(props);
     this.state = {
       breakfast: [],
+      newArray: [],
+
     }
+    this.onClickNewArray = this.onClickNewArray.bind(this);
   }
+  onClickNewArray(event) {
+    console.log(event.target, 'event')
+    // console.log(event.target.value, 'event');
+    const ArrayPedidos = this.state.newArray.push(event.target.name, event.target.value);
+    console.log(ArrayPedidos, 'arraypedidos')
+
+    //  this.state.newArray.push(event.target.value)
+    // db.collection('pedidos').add({
+    //   pedidos: this.state.newArray,
+    //   datatime: new Date(),
+    // })
+    //   .then((postNew) => {
+    //     const collecionPedidos = db.collection('pedidos');
+    //     const collecionPedidosOrdenada = collecionPedidos.orderBy('datatime', 'desc');
+    //     collecionPedidosOrdenada.get().then((element) => {
+    //       const pedidoNew = element.docs.map(doc => doc.data());
+    //       console.log(pedidoNew, 'Pedidos')
+    //       this.setState({
+    //         mensaje: [],
+    //         mostrarPedidos: pedidoNew,
+    //         newArray: this.state.newArray,
+    //       })
+    //     console.log(this.state.newArray, 'Se creo?')
+    //     })
+    //   })
+    //     .catch((error) => {
+    //       console.error('Error adding document: ', error);
+    //   });
+
+
+    // this.setState({newArray:event.target.value});
+    console.log(this.state.newArray, 'nuevo array')
+
+  }
+
   componentDidMount() {
     db.collection('desayuno').get().then((element) => {
-      const newBreakfast = element.docs.map(doc => doc.data());
+      var newBreakfast = element.docs.map(doc => doc.data());
       this.setState({
-        breakfast: newBreakfast
+        breakfast: newBreakfast,
       })
     })
   }
   render() {
     return (
       <div>
-        <table class="table table-dark">
+        <table className="table table-dark">
           <thead>
             <tr>
               <th scope="col">Nombre</th>
@@ -30,11 +68,11 @@ class Breakfast extends React.Component {
           </thead>
           {this.state.breakfast.map((element, key) => {
             return (
-              <tbody>
+              <tbody key={key}>
                 <tr>
-                  <th scope="row">{element.name}</th>
-                  <td>${element.price}</td>
-                  <td><button className="Agregar">Agregar</button></td>
+                  <th scope="row" onChange={this.onClickNewArray}>{element.name}</th>
+                  <td value={element.price} onChange={this.onClickNewArray}>${element.price}</td>
+                  <td><button name={element.name} value={element.price} className="Agregar" onClick={this.onClickNewArray}>Agregar</button></td>
                 </tr>
               </tbody>
             )
